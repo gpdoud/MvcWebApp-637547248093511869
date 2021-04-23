078e8dab4746172c5778e8c4bf1ff362a7a070ba
Greg Doud <gpdoud@gmail.com>
2021-04-22 21:47:01 -0400

    Committed by GitUtil

---
+++ MvcWebApp/DAL/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MvcWebApp.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebApp.DAL {
+    public class AppDbContext : DbContext {
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
